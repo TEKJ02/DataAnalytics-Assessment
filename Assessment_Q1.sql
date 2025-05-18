5c5095a1b03e7d410a5b8d9a6c53d5ccc56cd9ec
Emmanuel Kalada-Jamaica <139390465+TEKJ02@users.noreply.github.com>
2025-05-18 01:42:50 +0100

    Add files via upload

---
+++ Assessment_Q1.sql
@@ -0,0 +1,37 @@
+-- CTEs for selecting funded savings and investment plans
+WITH savings AS (
+    SELECT p.owner_id, 
+           COUNT(s.confirmed_amount) AS savings_count,
+           SUM(s.confirmed_amount) AS savings_amount
+    FROM adashi_staging.plans_plan AS p
+    INNER JOIN adashi_staging.savings_savingsaccount AS s 
+    ON p.id = s.plan_id
+    WHERE p.is_regular_savings = 1 AND s.confirmed_amount > 0
+    GROUP BY p.owner_id
+),
+
+investments AS (
+    SELECT p.owner_id, 
+           COUNT(s.confirmed_amount) AS investment_count,
+           SUM(s.confirmed_amount) AS investment_amount
+    FROM adashi_staging.plans_plan AS p
+    INNER JOIN adashi_staging.savings_savingsaccount AS s 
+    ON p.id = s.plan_id
+    WHERE p.is_a_fund = 1 AND s.confirmed_amount > 0
+    GROUP BY p.owner_id
+)
+
+SELECT 
+	u.id AS owner_id,
+    -- Join both first and last name of users
+    CONCAT(u.first_name, ' ', u.last_name) AS name,
+    s.savings_count,
+    i.investment_count,
+    ROUND(s.savings_amount + i.investment_amount, 2) AS total_deposits
+FROM savings AS s
+-- Join both CTEs
+INNER JOIN investments AS i 
+ON s.owner_id = i.owner_id
+INNER JOIN adashi_staging.users_customuser AS u 
+ON s.owner_id = u.id
+ORDER BY total_deposits DESC;
