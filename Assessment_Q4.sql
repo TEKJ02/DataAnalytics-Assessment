42375ae256e5f648382d9af1d9d5771a287585e7
Emmanuel Kalada-Jamaica <139390465+TEKJ02@users.noreply.github.com>
2025-05-19 18:39:11 +0100

    Assessment_Q4.sql

--- Assessment_Q4.sql
+++ Assessment_Q4.sql
@@ -1,18 +1,18 @@
 SELECT
 	u.id AS customer_id,
     
-    -- Join both first and last name of customers
-    CONCAT(u.first_name, ' ', u.last_name) AS name,
+    	-- Join both first and last name of customers
+    	CONCAT(u.first_name, ' ', u.last_name) AS name,
     
-    -- Calculate tenure of each customer in months
-    TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()) AS tenure_months,
-    COUNT(s.transaction_date) AS total_transactions,
+    	-- Calculate tenure of each customer in months
+    	TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()) AS tenure_months,
+    	COUNT(s.transaction_date) AS total_transactions,
 	
-    -- Calculate estimated CLV (rounded to 2 decimal places) 
-    ROUND(
-        (COUNT(s.transaction_date) / (TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()))) 
-        * 12 * AVG(0.001 * s.confirmed_amount), 2
-    ) AS estimated_clv
+    	-- Calculate estimated CLV (rounded to 2 decimal places) 
+    	ROUND(
+        	(COUNT(s.transaction_date) / (TIMESTAMPDIFF(MONTH, u.date_joined, CURDATE()))) 
+        	* 12 * AVG(0.001 * s.confirmed_amount), 2
+    	) AS estimated_clv
 FROM adashi_staging.users_customuser AS u
 
 -- Join savings table to the users table
